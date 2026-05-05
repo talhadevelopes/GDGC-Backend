c53f09411df6a9222bdcb6d5182ad973ed9e2f95
Daaniyah Khan <djk06khan@gmail.com>
2026-05-05 17:58:08 +0530

    leetcodee leaderboardddd

--- src/routes/leetcode.js
+++ src/routes/leetcode.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { getLeaderBoard } from "../controllers/LeetcodeController.js";
+import { VerifyToken } from "../middleware/AuthMiddleware.js";
+
 
 const leaderboardrouter = express.Router();
 
-leaderboardrouter.get("/",getLeaderBoard);
+// leaderboardrouter.get("/",getLeaderBoard);
+leaderboardrouter.get("/", VerifyToken, getLeaderBoard);
 
 export default leaderboardrouter;
